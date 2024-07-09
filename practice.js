efe83c5612a32752de0259a708fbffd6dbdf181d
fonsirada <fonsirada13@gmail.com>
2024-07-08 21:13:33 -0400

    starting javascript

---
+++ practice.js
@@ -0,0 +1,28 @@
+/*    Kelvin to celsius to fahrenheit practice project
+
+    // this is the temperature in kelvin, it will not change
+    const kelvin = 293;
+
+    // this is the temperature in celsius, it is 273 degrees less than kelvin
+    let celsius = kelvin - 273;
+
+    // this is the temperature in fahrenheit
+    let fahrenheit = celsius * (9/5) + 32;
+    // rounding the value down to a whole number
+    fahrenheit = Math.floor(fahrenheit);
+
+    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+*/
+
+// program that calculates your age in dog years
+let myAge = 18;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "Alfonso".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
